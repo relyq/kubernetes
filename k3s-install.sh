f5c30c542699ef358950fcd0b59b7fe6418f8c2d
relyq <relyqx@gmail.com>
2025-06-18 14:41:55 -0300

    install script update

--- k3s-install.sh
+++ k3s-install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+set -euo pipefail
+
+if [ "$EUID" -ne 0 ]; then
+  echo "please run as root"
+  exit 1
+fi
+
+if ! command -v flux &> /dev/null; then
+  echo "flux not installed, exiting"
+  exit 1
+fi
+
+
 cd "$(dirname "$0")"
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable traefik" sh -
@@ -11,14 +24,18 @@ systemctl disable k3s
 
 flux install
 
-kubectl create secret generic sops-age \
-	--namespace=flux-system \
-	--from-file=age.agekey=./age.key
+if ! kubectl get secret sops-age -n flux-system &> /dev/null; then
+  kubectl create secret generic sops-age \
+    --namespace=flux-system \
+    --from-file=age.agekey=./age.key
+fi
 
-kubectl create secret generic flux-system \
-  --namespace=flux-system \
-  --from-file=identity=/home/relyq/.ssh/id_ed25519 \
-  --from-file=identity.pub=/home/relyq/.ssh/id_ed25519.pub \
-  --from-literal=known_hosts="$(ssh-keyscan github.com)"
+if ! kubectl get secret flux-system -n flux-system &> /dev/null; then
+  kubectl create secret generic flux-system \
+    --namespace=flux-system \
+    --from-file=identity=/home/relyq/.ssh/id_ed25519 \
+    --from-file=identity.pub=/home/relyq/.ssh/id_ed25519.pub \
+    --from-literal=known_hosts="$(ssh-keyscan github.com)"
+fi
 
 kubectl apply -k ./clusters/production/flux-system
